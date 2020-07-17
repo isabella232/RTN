bcf68e203fd1119dfa6c08e837b87a41a04ebdb1
golestm <44486260+golestm@users.noreply.github.com>
2020-07-17 09:26:48 -0700

    Update CUST_RTN_ETL_STATIC_DATA.py

--- etl/CUST_RTN_ETL_STATIC_DATA.py
+++ etl/CUST_RTN_ETL_STATIC_DATA.py
@@ -121,7 +121,7 @@ print("Initial Static Tables Reloaded!  " + timestampStr)
 # Printing the Load Summary Stats
 #############################################################
 
-pd.read_sql('DATABASE '+SchemaName,con)
+pda = pd.read_sql('DATABASE '+SchemaName,con)
 
 query = "SELECT 'Python' as Process_Name, 'Static' as Table_Type, 'DIM_GEO_LOCATION_T' as TableName, count(*) as Records_Processed, max(current_timestamp(0)) as Process_Dttm FROM DIM_GEO_LOCATION_T GROUP BY 1,2,3 \
 UNION \
