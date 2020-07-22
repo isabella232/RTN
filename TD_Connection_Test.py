f4266245fa32ae7abdd4265e1a2acb1e79078cd3
golestm <44486260+golestm@users.noreply.github.com>
2020-07-21 17:32:57 -0700

    Add files via upload

---
+++ TD_Connection_Test.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]
+
+
+#!/usr/bin/env python
+# coding: utf-8
+
+
+#############################################################
+# Credentials
+#############################################################
+MyHost ='???'
+MyUser ='???'
+TempDB ='???'
+SchemaName = '???'
+Password = '???'
+
+#############################################################
+# Libraries
+#############################################################
+#pip install teradataml
+from teradataml.dataframe.dataframe import DataFrame, in_schema
+from teradataml.dataframe.copy_to import copy_to_sql
+from teradataml.context.context import create_context, remove_context, get_connection, get_context
+from teradataml.options.display import display
+from teradataml.dataframe.fastload import fastload
+from teradataml.context.context import *
+import pandas as pd
+con = create_context(host=MyHost, username=MyUser, password=Password, temp_database_name=TempDB, logmech='LDAP')
+
+#############################################################
+# Teradata Query Test
+#############################################################
+
+query = "Select * from DBC.DBCInfo"
+
+#Fetch the data from Teradata using Pandas Dataframe
+pda = pd.read_sql(query,con)
+print(pda)
+
